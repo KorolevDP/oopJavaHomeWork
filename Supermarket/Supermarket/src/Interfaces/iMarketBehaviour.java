21339169bb164441f2044cfaf29ae3396b3f11da
korolevdp <korolevdp-228@yandex.ru>
2023-07-02 18:35:13 +0400

    Added Supermarket HW_2

--- Supermarket/Supermarket/src/Interfaces/iMarketBehaviour.java
+++
@@ -1,9 +0,0 @@
-package Interfaces;
-
-import java.util.List;
-
-public interface iMarketBehaviour {
-    void acceptToMarket(iActorBehaviuor iActorBehaviuor);
-    void releaseFromMarket(List<Classes.Actor> actor);
-    void update();
-}
--- Supermarket/Supermarket/src/Interfaces/iQueueBehaviour.java
+++
@@ -1,9 +0,0 @@
-package Interfaces;
-
-public interface iQueueBehaviour {
-
-    void  takeInQueue(iActorBehaviuor iActorBehaviuor);
-    void releaseFromQueue();
-    void takeOrder();
-    void giveOrder();
-}
